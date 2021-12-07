c6465ff4b370b24fd74a517f8841f63a20cdc993
Erik Serrander <s7rul@ludd.ltu.se>
2021-12-07 09:12:43 +0100

    thing

--- LabB/MatlabAndSimulink/LabB_ControllerOverSimulator_Continuous_Parameters.m
+++ LabB/MatlabAndSimulink/LabB_ControllerOverSimulator_Continuous_Parameters.m
@@ -3,6 +3,95 @@ clear all;
 clc;
 
 
-K = [ -10.0000  -57.4908 -105.0371  -19.5009 ]
+%K = [ -10.0000  -57.4908 -105.0371  -19.5009 ]
 
 
+% Magical stuff that might make it work?
+clear all
+clc
+
+% Define constants.
+g = 9.8;
+b_f = 0;
+m_b = 0.381;
+l_b = 0.112;
+I_b = 0.00616;
+m_w = 0.036;
+l_w = 0.021;
+I_w = 0.00000746;
+R_m = 4.4;
+L_m = 0;
+b_m = 0;
+K_e = 0.444;
+K_t = 0.470;
+
+% Gamma matrix coeff.
+L22 = I_w/l_w + l_w * m_w + l_w * m_b;
+L24 = l_w * m_b * l_b;
+L42 = m_b * l_b;
+L44 = I_b + m_b * l_b^(2);
+
+% Gamma matrix.
+L = [1, 0, 0, 0;
+    0, L22, 0, L24;
+    0, 0, 1, 0;
+    0, L42, 0, L44];
+
+% Alfa matrix coeff.
+a22 = -(K_e * K_t) / (R_m * l_w);
+a24 = (K_e *K_t) / R_m;
+a42 = - a22;
+a43 = m_b * l_b * g;
+a44 = - a24;
+
+% Alfa matrix.
+Apre = [ 0, 1, 0, 0;
+    0, a22, 0, a24;
+    0, 0, 0, 1;
+    0, a42, a43, a44];
+
+% Derive correct A-matrix.
+A = inv(L) * Apre;
+
+% Beta matrix for simulation.
+Bpre = [ 
+    0, 0;
+    K_t / R_m, l_w;
+    0, 0;
+    -K_t/R_m, l_b
+    ];
+
+% Beta matrix.
+BpreAlt = [ 0; K_t / R_m; 0; -K_t/R_m];
+
+
+% Derive correct B-matrix.
+B = inv(L) * BpreAlt;
+
+% C-matrix
+C = [20, .1, 5, .2];
+
+
+% D-matrix.
+DAlt = [0, 0];
+D = 0;
+
+% Get transfer function.
+[num, den] = ss2tf(A, B, C, D, 1);
+
+s = tf('s');
+G1 = (s^4*num(1) + s^3*num(2) + s^2*num(3) + s*num(4) + num(5))/(s^4*den(1) + s^3*den(2) + s^2*den(3) + s*den(4) + den(5));
+G2 = (s^4*num(1) - s^3*num(2) + s^2*num(3) - s*num(4) + num(5))/(s^4*den(1) - s^3*den(2) + s^2*den(3) - s*den(4) + den(5));
+%G1 = -(s+2)/(s^2-1);
+%G2 = -(-s + 2) / (s^2-1);
+
+
+rlocus(G1 * G2)
+
+% State space to zero-pole conversion.
+%[zeroes, poles, gain] = ss2zp(A, B, C, DAlt, 1);
+
+R = 1;
+rho = 6;
+Q = rho*C'*C;
+K = lqr(A,B,Q,R);
--- LabB/MatlabAndSimulink/LabB_ObserverOverSimulator_Continuous_Parameters.m
+++ LabB/MatlabAndSimulink/LabB_ObserverOverSimulator_Continuous_Parameters.m
@@ -3,22 +3,138 @@ clear all;
 clc;
 
 
-LabB_Solutions
-
-
 % load the PID
 kP = -46.6;
 kI = -260;
 kD = -0.1;
 
+% Define constants.
+g = 9.8;
+b_f = 0;
+m_b = 0.381;
+l_b = 0.112;
+I_b = 0.00616;
+m_w = 0.036;
+l_w = 0.021;
+I_w = 0.00000746;
+R_m = 4.4;
+L_m = 0;
+b_m = 0;
+K_e = 0.444;
+K_t = 0.470;
 
-% 
+% Gamma matrix coeff.
+L22 = I_w/l_w + l_w * m_w + l_w * m_b;
+L24 = l_w * m_b * l_b;
+L42 = m_b * l_b;
+L44 = I_b + m_b * l_b^(2);
+
+% Gamma matrix.
+L = [1, 0, 0, 0;
+    0, L22, 0, L24;
+    0, 0, 1, 0;
+    0, L42, 0, L44];
+
+% Alfa matrix coeff.
+a22 = -(K_e * K_t) / (R_m * l_w);
+a24 = (K_e *K_t) / R_m;
+a42 = - a22;
+a43 = m_b * l_b * g;
+a44 = - a24;
+
+% Alfa matrix.
+Apre = [ 0, 1, 0, 0;
+    0, a22, 0, a24;
+    0, 0, 0, 1;
+    0, a42, a43, a44];
+
+% Derive correct A-matrix.
+A = inv(L) * Apre;
+
+
+% Beta matrix.
+BpreAlt = [ 0; K_t / R_m; 0; -K_t/R_m];
+
+
+% Derive correct B-matrix.
+B = inv(L) * BpreAlt;
+
+% C-matrix
+C = [1, 0, 0, 0;
+    0, 0, 1, 0];
+C_line = [20, .1, 5, .2];
+
+
+% D-matrix.
+D = 0;
+
+% State space to zero-pole conversion.
+%[zeroes, poles, gain] = ss2zp(A, B, C, DAlt, 1);
+R = 1;
+rho = 6;
+Q = rho*C_line'*C_line;
+[K, S, e] = lqr(A,B,Q,R);
+
+pc = e;
+pe = e*5;
+
+% Full Observer
+Lt = place(A', C', pe);
+L = Lt';
+
+% Partial observer
+% change of base
+TInv = [1, 0, 0, 0;
+    0, 0, 1, 0;
+    0, 1, 0, 0;
+    0, 0, 0, 1]
+
+T = inv(TInv);
+
+A_tilde = TInv * A * T;
+B_tilde = TInv * B;
+C_tilde = C * T;
+
+V = [0, 1, 0, 0;
+    0, 0, 0, 1];
+
+Ayy = A_tilde(1, 1);
+Ayx = A_tilde(1, [2, 3, 4]);
+Axy = A_tilde([2, 3, 4], 1);
+Axx = A_tilde([2, 3, 4], [2, 3, 4]);
+
+By = B_tilde(1);
+Bx = B_tilde(2:4);
+
+Cy = C_tilde([1, 2], 1);
+Cx = C_tilde([1, 2], [2, 3, 4]);
+
+CC = [Ayx; Cx];
+
+%Lt_p = place(Axx', ([Ayx; Cx])', pe([1, 2, 3]));
+%L_p = Lt_p';
+L_p = (place(Axx', Cx', pe(1, 2, 3)))';
+
+L_p_acc = L_p([1:3], 1);
+%L_p_nacc = L_p([1:3], [1, 2]);
+L_p_nacc = L_p([1:3], 2);
+
+M1 = (Axx - L_p_acc * Ayx - L_p_nacc * Cx);
+M2 = (Bx - L_p_acc * By);
+M3 = (Axy - L_p_acc * Ayy - L_p_nacc * Cy);
+M4 = L_p_nacc;
+M5 = L_p_acc;
+M6 = T([1:4], 1);
+M7 = T([1:4], [2:4]);
+
+
+% Partial observer
 % M1 = 1.0e+03 * [
 %    -0.4358   -0.0072    0.0091
 %    -0.0011   -0.0340    0.0010
 %     1.8717   -0.0165   -0.0400
 % ]
-% 
+%  
 % M2 = [
 %    20.6000
 %          0
