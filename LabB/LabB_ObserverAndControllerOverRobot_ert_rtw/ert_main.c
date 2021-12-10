f3952d38e0648b8c8bd4ce32e18e7666da919877
zachtyll <zacharias.tyllstrom@gmail.com>
2021-12-10 16:12:16 +0100

    Added time and info from experiments as CSV.

---
+++ LabB/LabB_ObserverAndControllerOverRobot_ert_rtw/ert_main.c
@@ -0,0 +1,97 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: ert_main.c
+ *
+ * Code generated for Simulink model 'LabB_ObserverAndControllerOverRobot'.
+ *
+ * Model version                  : 8.1
+ * Simulink Coder version         : 9.4 (R2020b) 29-Jul-2020
+ * C/C++ source code generated on : Fri Dec 10 15:53:44 2021
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Atmel->AVR
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "LabB_ObserverAndControllerOverRobot.h"
+#include "rtwtypes.h"
+
+volatile int IsrOverrun = 0;
+static boolean_T OverrunFlag = 0;
+void rt_OneStep(void)
+{
+  /* Check for overrun. Protect OverrunFlag against preemption */
+  if (OverrunFlag++) {
+    IsrOverrun = 1;
+    OverrunFlag--;
+    return;
+  }
+
+#ifndef _MW_ARDUINO_LOOP_
+
+  sei();
+
+#endif;
+
+  LabB_ObserverAndControllerOverRobot_step();
+
+  /* Get model outputs here */
+#ifndef _MW_ARDUINO_LOOP_
+
+  cli();
+
+#endif;
+
+  OverrunFlag--;
+}
+
+volatile boolean_T stopRequested;
+volatile boolean_T runModel;
+int main(void)
+{
+  float modelBaseRate = 0.005;
+  float systemClock = 0;
+
+  /* Initialize variables */
+  stopRequested = false;
+  runModel = false;
+  init();
+  MW_Arduino_Init();
+  rtmSetErrorStatus(LabB_ObserverAndControllerOv_M, 0);
+  LabB_ObserverAndControllerOverRobot_initialize();
+  cli();
+  configureArduinoAVRTimer();
+  runModel =
+    rtmGetErrorStatus(LabB_ObserverAndControllerOv_M) == (NULL);
+
+#ifndef _MW_ARDUINO_LOOP_
+
+  sei();
+
+#endif;
+
+  sei();
+  while (runModel) {
+    stopRequested = !(
+                      rtmGetErrorStatus(LabB_ObserverAndControllerOv_M) == (NULL));
+    runModel = !(stopRequested);
+    MW_Arduino_Loop();
+  }
+
+  /* Disable rt_OneStep() here */
+
+  /* Terminate model */
+  LabB_ObserverAndControllerOverRobot_terminate();
+  cli();
+  return 0;
+}
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
