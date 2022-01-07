6385d06ec27b93696f75500cc3120ead409a9483
zachtyll <zacharias.tyllstrom@gmail.com>
2022-01-07 17:21:17 +0100

    Fixed typo, updated .mat.

--- LabB/mat_export.m
+++ LabB/mat_export.m
@@ -5,16 +5,16 @@ exp_1 = load('E:\MATLAB-Projects\R7003E\LabB\experiment_1.mat');
 exp_2 = load('E:\MATLAB-Projects\R7003E\LabB\experiment_2.mat');
 
 % Load times.
-group_12_experiment_1_times = exp_1.afTimes;
-group_12_experiment_2_times = exp_2.afTimes;
+group_12_experiment_1_times = exp_1.afTimes';
+group_12_experiment_2_times = exp_2.afTimes';
 
 % Load encoders.
 group_12_experiment_1_encoder = exp_1.aafProcessedInformation(2, 1:end)';
-group_12_experiment_2_encoder = exp_1.aafProcessedInformation(2, 1:end)';
+group_12_experiment_2_encoder = exp_2.aafProcessedInformation(2, 1:end)';
 
 % Load angles.
 group_12_experiment_1_angle = exp_1.aafProcessedInformation(3, 1:end)';
-group_12_experiment_2_angle = exp_1.aafProcessedInformation(3, 1:end)';
+group_12_experiment_2_angle = exp_2.aafProcessedInformation(3, 1:end)';
 
 % Load actuation.
 group_12_experiment_1_actuation = exp_1.aafProcessedInformation(1, 1:end)';
